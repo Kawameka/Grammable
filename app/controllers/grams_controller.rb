87f7641c04dc6c5c495189dd4bec09462b8d8486
Tanner <tanner.tannerlevine@gmail.com>
2017-08-20 02:24:16 +0000

    image uloading with carrierwave and s3

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,6 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -55,7 +56,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :image)
   end
 
   def render_status(status=:not_found)
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -3,4 +3,6 @@ class Gram < ApplicationRecord
 
   belongs_to :user
 
+  mount_uploader :image, ImageUploader
+
 end
